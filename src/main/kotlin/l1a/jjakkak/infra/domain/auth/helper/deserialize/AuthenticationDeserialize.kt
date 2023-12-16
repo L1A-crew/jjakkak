2fed3e823103780ef204f83889225b9565c859f3
yoonho <yoonho2015@gmail.com>
2023-12-16 22:30:26 +0900

    유저를 생성할 수 있다 및 도메인 모델 구조 변경
    1. 유저 생성 API
    2. 인증 로직에서 ByPassUrl 우회안되는 이슈 보완
    3. kotlin 버전 변경
    4. 도메인 모델에서 쓰기 모델, 읽기 모델 분리

--- src/main/kotlin/l1a/jjakkak/infra/domain/auth/helper/deserialize/AuthenticationDeserialize.kt
+++ src/main/kotlin/l1a/jjakkak/infra/domain/auth/helper/deserialize/AuthenticationDeserialize.kt
@@ -1,12 +1,13 @@
 package l1a.jjakkak.infra.domain.auth.helper.deserialize
 
-import l1a.jjakkak.core.domain.auth.Authentication
+import l1a.jjakkak.core.domain.auth.AuthenticationId
+import l1a.jjakkak.core.domain.auth.AuthenticationQuery
 import l1a.jjakkak.infra.domain.auth.entity.AuthenticationEntity
 
 interface AuthenticationDeserialize {
-    fun AuthenticationEntity.toDomain(): Authentication =
-        Authentication.create(
-            providerId = providerId,
+    fun AuthenticationEntity.toDomain(): AuthenticationQuery =
+        AuthenticationQuery.create(
+            id = AuthenticationId(authenticationId),
             type = type,
             createdAt = createdAt!!,
             updatedAt = updatedAt!!
--- src/main/kotlin/l1a/jjakkak/infra/domain/user/helper/deserialize/UserDeserialize.kt
+++ src/main/kotlin/l1a/jjakkak/infra/domain/user/helper/deserialize/UserDeserialize.kt
@@ -1,13 +1,14 @@
 package l1a.jjakkak.infra.domain.user.helper.deserialize
 
-import l1a.jjakkak.core.domain.user.User
+import l1a.jjakkak.core.domain.user.UserId
+import l1a.jjakkak.core.domain.user.UserQuery
 import l1a.jjakkak.infra.domain.auth.helper.deserialize.AuthenticationDeserialize
 import l1a.jjakkak.infra.domain.user.entity.UserEntity
 
 interface UserDeserialize: AuthenticationDeserialize {
-    fun UserEntity.toDomain(): User =
-        User.create(
-            id = userId,
+    fun UserEntity.toDomain(): UserQuery =
+        UserQuery.create(
+            id = UserId(userId!!),
             authentication = authenticationEntity.toDomain(),
             createdAt = createdAt!!,
             updatedAt = updatedAt!!
